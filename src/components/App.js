507e121e5abf184d3ea3ad948d64222319931cbe
Anjan <anjanlayek001@gmail.com>
2022-11-12 23:49:47 +0530

    committing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+   const [input1,setInput1] = useState('');
+   const [input2,setInput2] = useState('');
+   const [result,setResult] = useState(0);
+   
+   const firstInput = (e) => {
+    setInput1(e.target.value);
 
+   }
+   const secondInput = (e)=>{
+  setInput2(e.target.value);
+
+   }
+   useEffect(()=>{
+    setResult(Number(input1) + Number(input2))
+   })
+   
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' onChange={firstInput} value={input1} />
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' onChange={secondInput} value={input2}/> 
       
-      <p id='result'></p>
+      <p id='result'>{result}</p>
     </div>
   )
 }
